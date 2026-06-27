7253121b068d67217ba4d09bfa4f236eaf6520cc
Cristian Lopez <cristianlopezr.931@gmail.com>
2026-06-26 21:26:56 -0400

    Refactored dto validator errors

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 True Auth
+
+Dev mode
+
+1. Copy .env.template and replace the environment variables accordingly
+
+2. Run the db ```docker compose up -d```
--- src/presentation/middlewares/dto-validator.middleware.ts
+++ src/presentation/middlewares/dto-validator.middleware.ts
@@ -8,12 +8,28 @@ export class DtoValidator {
         return async (req: Request, res: Response, next: NextFunction) => {
             const dtoInstance = plainToInstance(dtoClass, req[propertyName])
             const errors = await validate(dtoInstance);
-
             if (errors.length > 0) {
-                return res.status(400).json({ errors })
+                const validationErrors = DtoValidator.formatErrors(errors)
+
+                return res.status(400).json({ statusCode: 400, message: 'Bad Request', errors: validationErrors })
             }
             req[propertyName] = dtoInstance;
             next();
         }
     }
+
+    private static formatErrors = (errors: any[]) => {
+        const formattedErrors: Record<string, any> = {};
+
+        errors.forEach((err) => {
+            if (err.constraints) {
+                formattedErrors[err.property] = Object.values(err.constraints);
+            }
+            if (err.children && err.children.length > 0) {
+                formattedErrors[err.property] = DtoValidator.formatErrors(err.children);
+            }
+        });
+
+        return formattedErrors;
+    };
 }
